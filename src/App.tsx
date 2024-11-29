537bb944aedeea6a71cf5a198636bd83f5a0ad72
ch22as <chmartin22@yahoo.es>
2024-11-29 21:10:14 +0100

    donw

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
-import "./App.css"
-import TypingTest from "./components/TypingTest"
+import { Typography } from '@mui/material';
+import Button from '@mui/material/Button';
 
 function App() {
   return (
-    <div>
-      < TypingTest />
-    </div>
+    <>
+    <Typography variant="h3" sx={{ py: 8 }}>Hola mundo desde material UI</Typography>
+    <Button size="large" variant="contained">Click</Button>
+    <Button size="medium" variant="outlined">Click</Button>
+    <Button size="small" variant="text">Click</Button>
+    </>
   )
 }
 
