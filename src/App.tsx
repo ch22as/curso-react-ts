2168ebce217214f8d4d407d6b68427b0e41a63e0
Christian Martin <c.martin@etecnic.es>
2024-12-06 12:50:08 +0100

    Renderizado condicional

--- src/App.tsx
+++ src/App.tsx
@@ -1,80 +1,32 @@
-import { useEffect, useRef, useState } from "react"
-import "../src/App.css"
+import { useState } from "react"
+import "./App.css"
+import { Home, About, Contact} from './views'
 
-interface Alumno {
-  id: number
-  nombre: string
-  nota: number
-}
 
-const alumnos: Alumno[] = [
-  {
-    id: 1,
-    nombre: 'Diego',
-    nota: 8
-  },
-  {
-    id: 2,
-    nombre: 'Antonio',
-    nota: 10
-  },
-  {
-    id: 3,
-    nombre: 'Adrian',
-    nota: 10
-  },
-  {
-    id: 4,
-    nombre: 'Albert',
-    nota: 8
-  }
-]
-
-// interface Todo {
-//   id: number | string,
-//   texto: string,
-//   completed: boolean
-// }
-
+type Route = "Home" | "About" | "Contact"
 
 const App = () => {
+  const RoutView = ({ routeName }: {routeName: Route}) => {
+    if(routeName === "Home") return <Home />
+    if(routeName === "About") return <About />
+    if(routeName === "Contact") return <Contact />
 
-  // const [todos, setTodos] = useState<Todo[]>([])
-  // const [inputValue, setInputValue] = useState("")
-  const [notaMinima, setNotaMinima] = useState(0)
-
-  const inputRef = useRef<HTMLInputElement>(null)
-
-  const actualizarNotaMinima = () => {
-    if(!inputRef.current?.value) return
-
-    const valorDelInput = Number(inputRef.current.value)
-
-    setNotaMinima(valorDelInput)
-
-    console.log(valorDelInput)
-    console.log(typeof valorDelInput)
+    return<h3>Ruta no encontrada</h3>
   }
 
+  const [route, setRoute] = useState<Route>("Contact")
 
   return (
     <>
-      <h1>Ejercicio de listas</h1>
-
-      <input
-        type="number"
-        ref={inputRef}
-        placeholder="Introduce la nota mínima"
-      />
-
-      <button onClick={actualizarNotaMinima}>Buscar</button>
-      <ul>
-        { alumnos.filter((alumno) => alumno.nota >= notaMinima).map((alumno) =>
-          <li key={alumno.id}>
-            {alumno.nombre}: {alumno.nota}
-          </li>
-        )}
-      </ul>
+    <RoutView routeName={route} />
+    {/* { route === "Home" ? <Home /> : null }
+    { route === "About" ? <About /> : null }
+    { route === "Contact" && <Contact /> } */}
+    <div style={ { marginTop: "1rem" } }>
+      <button onClick={ () => setRoute("Home") }>Ir a Home</button>
+      <button onClick={ () => setRoute("About") }>Ir a About</button>
+      <button onClick={ () => setRoute("Contact") }>Ir a Contact</button>
+    </div>
     </>
   )
 }
